6ef3fb2b099dbd759c84b7f6a5312ef863ad29da
Josh Nolan <joshnolan118@gmail.com>
2014-02-11 18:42:49 +0000

    calcDeets model push it real good

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -28,9 +28,7 @@ projectTurkey.controller('dataInput', ['$scope', 'SOC', function($scope, SOC){
       'gender': 1
     }
   ];
-
   $scope.jobCodesTemp = [];
-
   $scope.writing = 0;
 
   $scope.getSoc0 = function() {
@@ -49,3 +47,75 @@ projectTurkey.controller('dataInput', ['$scope', 'SOC', function($scope, SOC){
   };
 
 }]);
+
+
+projectTurkey.controller('calculateDeets', ['$scope', 'SOC', function($scope, SOC)
+{
+
+  $scope.wages = [
+  {
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }, 
+    {
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+  ];
+
+  $scope.prospects = [
+  {
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }, 
+    {
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+  ];
+
+  $scope.getWagesFor0 = function() {
+    SOC.get('http://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=" + $scope.users[0].jobCode + "&coarse=true&filter=gender:" + gender + "&breakdown=region')    
+    .sucess(function(data)
+    {
+      console.log(data);
+      angular.forEach(data.series[0].breakdown, function(i,e)
+      {
+        $scope.wages[0][e.region] = e.estpay;
+      });
+    };
+  };
+
+  $scope.getWagesFor1 = function()
+  {
+    SOC.get('http://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=' + $scope.users[0].jobCode + '&coarse=true&filter=gender:' + gender + '&breakdown=region')    
+    .sucess(function(data)
+    {
+      angular.forEach(data.series[0].breakdown, function(i,e)
+      {
+        $scope.wages[1][e.region] = e.estpay;
+      });
+    };
+  };
+
+  $scope.getProspectsFor0 = function()
+  {
+    SOC.get('http://api.lmiforall.org.uk/api/v1/wf/predict/breakdown/region?soc=' + soc + '&maxYear=2015')    
+    .sucess(function(data)
+    {
+      angular.forEach(data.predictedEmployment[0].breakdown, function(i,e)
+      {
+        $scope.prospects[0][e.code] = e.estpay;
+      });
+    };
+  };
+
+  $scope.getProspectsFor1 = function()
+  {
+    SOC.get('http://api.lmiforall.org.uk/api/v1/wf/predict/breakdown/region?soc=' + soc + '&maxYear=2015')    
+    .sucess(function(data)
+    {
+      angular.forEach(data.predictedEmployment[0].breakdown, function(i,e)
+      {
+        $scope.prospects[1][e.code] = e.estpay;
+      });
+    };
+  };
+}]);
+
