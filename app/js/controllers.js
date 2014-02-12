b675e271da67a4a14661f01aa06059d6ab483e2e
Josh Nolan <joshnolan118@gmail.com>
2014-02-12 17:10:41 +0000

    education metric and getLifePlan()

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,6 @@
 'use strict';
 
 /* Controllers */
-
 var projectTurkey = angular.module('projectTurkey', []);
 
 projectTurkey.factory('SOC', ['$http', function ($http) {
@@ -51,6 +50,7 @@ projectTurkey.service('appData', function(){
 projectTurkey.controller('dataInput', ['$scope', 'SOC', 'appData', function($scope, SOC, appData){
 
   $scope.users = appData.users;
+  $scope.metrics = appData.metrics;
   $scope.jobCodesTemp = [];
   $scope.writing = 0;
 
@@ -87,8 +87,9 @@ projectTurkey.controller('usa', ['$scope', 'SOC', 'appData', function($scope, SO
 }])
 
 projectTurkey.controller('calculateDeets', ['$scope', 'SOC', 'appData', function($scope, SOC, appData){
-
+  
   $scope.users = appData.users;
+  $scope.metrics = appData.metrics;
 
   $scope.wages = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -100,11 +101,21 @@ projectTurkey.controller('calculateDeets', ['$scope', 'SOC', 'appData', function
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
 
+  $scope.education = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
+  $scope.resultMatrix =[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
+  $scope.message = "";
+
   $scope.getWages = function(){
+    console.log($scope.metrics);
     $scope.getWagesFor0();
     $scope.getWagesFor1();
     $scope.getProspectsFor0();
     $scope.getProspectsFor1();
+    $scope.getEducation();
   }
 
   $scope.getWagesFor0 = function() {
@@ -155,5 +166,84 @@ projectTurkey.controller('calculateDeets', ['$scope', 'SOC', 'appData', function
       });
   };
 
+  $scope.getEducation = function(){
+    SOC.get('http://opendatacommunities.org/sparql.json?query=PREFIX+finance%3A+%3Chttp%3A%2F%2Fopendatacommunities.org%2Fdef%2Ffinance%2F%3E%0D%0APREFIX+qb%3A+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23%3E%0D%0APREFIX+housing%3A+%3Chttp%3A%2F%2Fopendatacommunities.org%2Fdef%2Fhousing%2F%3E%0D%0APREFIX+gov%3A+%3Chttp%3A%2F%2Fopendatacommunities.org%2Fdef%2Flocal-government%2F%3E%0D%0APREFIX+sdmx%3A+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23%3E%0D%0A%0D%0A%0D%0Aselect+%3Fregion+%28SUM%28%3Fspend%29+as+%3Fs%29+where+%7B%0D%0A%3Fobs+finance%3Aauthority+%3Fdistricts+.%0D%0A%3Fdistricts+%3Chttp%3A%2F%2Fstatistics.data.gov.uk%2Fdef%2Fadministrative-geography%2Fregion%3E+%3Fregion+.%0D%0A%3Fobs+finance%3AserviceExpenditureCategory+%3Chttp%3A%2F%2Fopendatacommunities.org%2Fdef%2Ffinance%2Fconcept%2Fservice-expenditure-category%2F190%3E+.%0D%0A%3Fobs+qb%3AdataSet+%3Chttp%3A%2F%2Fopendatacommunities.org%2Fdata%2Fservice-expenditure%3E+.%0D%0A%3Fobs+finance%3ArevenueAccountBudgetCategory+%3Chttp%3A%2F%2Fopendatacommunities.org%2Fdef%2Ffinance%2Fconcept%2Frevenue-account-budget-category%2Fnet-total-cost%3E+.%0D%0A%3Fobs+finance%3Aamount+%3Fspend+.%0D%0A%3Fh+qb%3AdataSet+%3Chttp%3A%2F%2Fopendatacommunities.org%2Fdata%2Fhouseholds-2008%3E+.%0D%0A%3Fh+sdmx%3ArefArea+%3Farea+.%0D%0A%3Farea+gov%3AisGovernedBy+%3Fdistricts+.%0D%0A%3Fh+housing%3Ahouseholds+%3Fhouseholds%0D%0A%7D%0D%0Agroup+by+%3Fregion')
+      .success(function(data){
+        angular.forEach(data.results.bindings, function (object, key) {
+          var URI = object.region.value;
+          var index = $scope.regionLetterToRegionIndex(URI.substring(URI.length-1));
+          if (index != -1)
+          {
+            $scope.education[index] = parseInt(object.s.value);
+          }
+        });        
+      })
+      .error(function(data){
+        console.error("Error:", data);
+      });
+  };
+
+  $scope.getLifePlan = function()
+  {
+    var bestRegion = -1;
+    var bestRegionValue = -1;
+    for(var index = 0; index < $scope.resultMatrix.length-1; index++)
+    {
+      var val = 0;
+      val += $scope.wages[0][index] * $scope.metrics.wages;
+      val += $scope.wages[1][index] * $scope.metrics.wages;
+      val += $scope.prospects[0][index] * $scope.metrics.prospects;
+      val += $scope.prospects[1][index] * $scope.metrics.prospects;
+      val += $scope.education[index]    * $scope.metrics.education;
+      console.log(val);
+      $scope.resultMatrix[index] = val;    
+      
+      if (val > bestRegionValue)
+      {
+        bestRegion = index;
+        bestRegionValue = val;
+      }
+    }
+    $scope.message = "You should move to:" + $scope.regionIndexToRegionName(bestRegion);
+  };
+
+  // TODO: Make sure we are using the regional indexes correct, LMI have conflicting values?! :(
+  $scope.regionLetterToRegionIndex = function(letter) {
+    var l = letter;
+    switch(l.toLowerCase())
+    {
+      case 'h': return 0; // London
+      case 'a': return 1; // North East
+      case 'b': return 2; // North West
+      case 'd': return 4; // Yorkshire & Humberside
+      case 'e': return 5; // East Midlands
+      case 'f': return 6; // West Midlands
+      case 'g': return 7; // Eastern
+      case 'j': return 9; // South East
+      case 'k': return 10;// South West
+      default: return -1; // sorry: Scotland, Wales, NI
+    };
+  };
+
+  $scope.regionIndexToRegionName = function(index)
+  {
+    switch(index)
+    {
+      case 0: return 'London';
+      case 1: return 'North East';
+      case 2: return  'North West';
+      case 3: return '????';
+      case 4: return 'Yorkshire & Humberside';
+      case 5: 'East Midlands';
+      case 6: 'West Midlands';
+      case 7: 'Eastern';
+      case 8: '?????';
+      case 9: 'South East';
+      case 10: 'South West';
+      case 10: return 'Wales';
+      case 12: return 'Scotland';
+      case 13: return 'Northern Ireland';
+    };
+  };
 }]);
 
