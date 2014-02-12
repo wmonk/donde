4e0a08f386cdf4c3d733c6e2f2ffd865358ef799
William Monk <Will@Williams-MacBook-Pro.local>
2014-02-12 10:41:52 +0000

    - index.html Added bindings for wages and prospects
    - controller.js
    Added functions to get data from wages/prospects API endpoints, and put them into the array.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,110 +12,148 @@ projectTurkey.factory('SOC', ['$http', function ($http) {
   };
 }]);
 
-projectTurkey.controller('dataInput', ['$scope', 'SOC', function($scope, SOC){
-
-  $scope.users = [
+projectTurkey.service('appData', function(){
+  // this.users = [
+  //   {
+  //     'age': 0,
+  //     'jobTitle' : '',
+  //     'jobCode': 0,
+  //     'gender': 1
+  //   },
+  //   {
+  //     'age': 0,
+  //     'jobTitle' : '',
+  //     'jobCode': 0,
+  //     'gender': 1
+  //   }
+  // ];
+  this.users = [
     {
-      'age': 0,
+      'age': 23,
       'jobTitle' : '',
-      'jobCode': 0,
+      'jobCode': 3421,
       'gender': 1
     },
     {
-      'age': 0,
+      'age': 22,
       'jobTitle' : '',
-      'jobCode': 0,
-      'gender': 1
+      'jobCode': 2472,
+      'gender': 2
     }
   ];
+  this.metrics = {
+      'wages': 0,
+      'prospects': 0,
+      'education': 0
+    };
+})
+
+projectTurkey.controller('dataInput', ['$scope', 'SOC', 'appData', function($scope, SOC, appData){
+
+  $scope.users = appData.users;
   $scope.jobCodesTemp = [];
   $scope.writing = 0;
 
   $scope.getSoc0 = function() {
     SOC.get('http://api.lmiforall.org.uk/api/v1/soc/search?q=' + $scope.users[0].jobTitle)
     .success(function(data) {
-      console.log(data);
       $scope.jobCodesTemp[0] = data;
     });
   };
   $scope.getSoc1 = function() {
     SOC.get('http://api.lmiforall.org.uk/api/v1/soc/search?q=' + $scope.users[1].jobTitle)
     .success(function(data) {
-      console.log(data);
       $scope.jobCodesTemp[1] = data;
     });
   };
 
 }]);
 
+projectTurkey.controller('usa', ['$scope', 'SOC', 'appData', function($scope, SOC, appData){
+  $scope.users = appData.users;
+  $scope.metrics = appData.metrics;
 
-projectTurkey.controller('calculateDeets', ['$scope', 'SOC', function($scope, SOC)
-{
+  $scope.maxTokens = 14;
+  $scope.tokensUsed = 0;
+  $scope.maxWages = 15;
+  $scope.maxProspects = 15;
+  $scope.maxEducation = 15;
+  $scope.limitTokens = function(){
+    $scope.maxWages = (parseInt($scope.maxTokens) - parseInt($scope.tokensUsed)) + parseInt($scope.metrics.wages);
+    $scope.maxProspects = (parseInt($scope.maxTokens) - parseInt($scope.tokensUsed)) + parseInt($scope.metrics.prospects);
+    $scope.maxEducation = (parseInt($scope.maxTokens) - parseInt($scope.tokensUsed)) + parseInt($scope.metrics.education);
+    $scope.tokensUsed = parseInt($scope.metrics.wages) + parseInt($scope.metrics.prospects) + parseInt($scope.metrics.education);
+  }
+}])
+
+projectTurkey.controller('calculateDeets', ['$scope', 'SOC', 'appData', function($scope, SOC, appData){
+
+  $scope.users = appData.users;
 
   $scope.wages = [
-  {
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    }, 
-    {
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    }
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
 
   $scope.prospects = [
-  {
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    }, 
-    {
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    }
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
 
+  $scope.getWages = function(){
+    $scope.getWagesFor0();
+    $scope.getWagesFor1();
+    $scope.getProspectsFor0();
+    $scope.getProspectsFor1();
+  }
+
   $scope.getWagesFor0 = function() {
-    SOC.get('http://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=" + $scope.users[0].jobCode + "&coarse=true&filter=gender:" + gender + "&breakdown=region')    
-    .sucess(function(data)
-    {
-      console.log(data);
-      angular.forEach(data.series[0].breakdown, function(i,e)
-      {
-        $scope.wages[0][e.region] = e.estpay;
+    SOC.get('http://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=' + $scope.users[0].jobCode + '&coarse=true&filter=gender:' + $scope.users[0].gender + '&breakdown=region')
+      .success(function(data){
+        angular.forEach(data.series[0].breakdown, function(e, i){
+          $scope.wages[0][e.region] = e.estpay;
+        });
+      })
+      .error(function(data){
+        console.error("Error:", data);
       });
-    };
   };
 
-  $scope.getWagesFor1 = function()
-  {
-    SOC.get('http://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=' + $scope.users[0].jobCode + '&coarse=true&filter=gender:' + gender + '&breakdown=region')    
-    .sucess(function(data)
-    {
-      angular.forEach(data.series[0].breakdown, function(i,e)
-      {
-        $scope.wages[1][e.region] = e.estpay;
+  $scope.getWagesFor1 = function(){
+    SOC.get('http://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=' + $scope.users[1].jobCode + '&coarse=true&filter=gender:' + $scope.users[1].gender + '&breakdown=region')
+      .success(function(data){
+        angular.forEach(data.series[0].breakdown, function(e, i){
+          $scope.wages[1][e.region] = e.estpay;
+        });
+      })
+      .error(function(data){
+        console.error("Error:", data);
       });
-    };
   };
 
-  $scope.getProspectsFor0 = function()
-  {
-    SOC.get('http://api.lmiforall.org.uk/api/v1/wf/predict/breakdown/region?soc=' + soc + '&maxYear=2015')    
-    .sucess(function(data)
-    {
-      angular.forEach(data.predictedEmployment[0].breakdown, function(i,e)
-      {
-        $scope.prospects[0][e.code] = e.estpay;
+  $scope.getProspectsFor0 = function(){
+    SOC.get('http://api.lmiforall.org.uk/api/v1/wf/predict/breakdown/region?soc=' + $scope.users[1].jobCode + '&maxYear=2015')
+      .success(function(data){
+        angular.forEach(data.predictedEmployment[0].breakdown, function(e, i){
+          $scope.prospects[0][e.code] = e.employment;
+        });
+      })
+      .error(function(data){
+        console.error("Error:", data);
       });
-    };
   };
 
-  $scope.getProspectsFor1 = function()
-  {
-    SOC.get('http://api.lmiforall.org.uk/api/v1/wf/predict/breakdown/region?soc=' + soc + '&maxYear=2015')    
-    .sucess(function(data)
-    {
-      angular.forEach(data.predictedEmployment[0].breakdown, function(i,e)
-      {
-        $scope.prospects[1][e.code] = e.estpay;
+  $scope.getProspectsFor1 = function(){
+    SOC.get('http://api.lmiforall.org.uk/api/v1/wf/predict/breakdown/region?soc=' + $scope.users[1].jobCode + '&maxYear=2015')
+      .success(function(data){
+        angular.forEach(data.predictedEmployment[0].breakdown, function(e, i){
+          $scope.prospects[1][e.code] = e.employment;
+        });
+      })
+      .error(function(data){
+        console.error("Error:", data);
       });
-    };
   };
+
 }]);
 
