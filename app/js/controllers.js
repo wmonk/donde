5562bd7f7c5a1c45e228a195f2e795fdfe0fde38
Josh Nolan <joshnolan118@gmail.com>
2014-02-16 17:11:16 +0000

    chart FUNZIES

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* Controllers */
-var projectTurkey = angular.module('projectTurkey', []);
+var projectTurkey = angular.module('projectTurkey',['googlechart']);
 
 projectTurkey.factory('SOC', ['$http', function ($http) {
   return {
@@ -280,122 +280,57 @@ projectTurkey.controller('calculateDeets', ['$scope', 'SOC', 'appData', function
     };
   };
 
+     var chart1 = {};
+    chart1.type = "ColumnChart";
+    chart1.cssStyle = "height:200px; width:300px;";
+    chart1.data = {"cols": [
+        {id: "month", label: "Month", type: "string"},
+        {id: "laptop-id", label: "Laptop", type: "number"},
+        {id: "desktop-id", label: "Desktop", type: "number"},
+        {id: "server-id", label: "Server", type: "number"},
+        {id: "cost-id", label: "Shipping", type: "number"}
+    ], "rows": [
+        {c: [
+            {v: "January"},
+            {v: 19, f: "42 items"},
+            {v: 12, f: "Ony 12 items"},
+            {v: 7, f: "7 servers"},
+            {v: 4}
+        ]},
+        {c: [
+            {v: "February"},
+            {v: 13},
+            {v: 1, f: "1 unit (Out of stock this month)"},
+            {v: 12},
+            {v: 2}
+        ]},
+        {c: [
+            {v: "March"},
+            {v: 24},
+            {v: 0},
+            {v: 11},
+            {v: 6}
 
-  $scope.chart = {
-  "type": "AreaChart",
-  "displayed": true,
-  "data": {
-    "cols": [
-      {
-        "id": "month",
-        "label": "Month",
-        "type": "string",
-        "p": {}
-      },
-      {
-        "id": "laptop-id",
-        "label": "Laptop",
-        "type": "number",
-        "p": {}
-      },
-      {
-        "id": "desktop-id",
-        "label": "Desktop",
-        "type": "number",
-        "p": {}
-      },
-      {
-        "id": "server-id",
-        "label": "Server",
-        "type": "number",
-        "p": {}
-      },
-      {
-        "id": "cost-id",
-        "label": "Shipping",
-        "type": "number"
-      }
-    ],
-    "rows": [
-      {
-        "c": [
-          {
-            "v": "January"
-          },
-          {
-            "v": 19,
-            "f": "42 items"
-          },
-          {
-            "v": 12,
-            "f": "Ony 12 items"
-          },
-          {
-            "v": 7,
-            "f": "7 servers"
-          },
-          {
-            "v": 4
-          }
-        ]
-      },
-      {
-        "c": [
-          {
-            "v": "February"
-          },
-          {
-            "v": 13
-          },
-          {
-            "v": 1,
-            "f": "1 unit (Out of stock this month)"
-          },
-          {
-            "v": 15
-          },
-          {
-            "v": 2
-          }
-        ]
-      },
-      {
-        "c": [
-          {
-            "v": "March"
-          },
-          {
-            "v": 24
-          },
-          {
-            "v": 5
-          },
-          {
-            "v": 11
-          },
-          {
-            "v": 6
-          }
-        ]
-      }
-    ]
-  },
-  "options": {
-    "title": "Sales per month",
-    "isStacked": "true",
-    "fill": 20,
-    "displayExactValues": true,
-    "vAxis": {
-      "title": "Sales unit",
-      "gridlines": {
-        "count": 28
-      }
-    },
-    "hAxis": {
-      "title": "Date"
-    }
-  },
-  "formatters": {}
-};
+        ]}
+    ]};
+
+    chart1.options = {
+        "title": "Sales per month",
+        "isStacked": "true",
+        "fill": 20,
+        "displayExactValues": true,
+        "vAxis": {
+            "title": "Sales unit", "gridlines": {"count": 6}
+        },
+        "hAxis": {
+            "title": "Date"
+        }
+    };
+
+    chart1.formatters = {};
+
+    $scope.chart = chart1;
+
+
 }]);
 
